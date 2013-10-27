58118d7dff6310e53c7886fd6c949eeb16aa649f
Novacha Man <novachalon@gmail.com>
2013-10-27 11:57:53 +1100

    Updated django packages

--- src/server/Server/settings.py
+++ src/server/Server/settings.py
@@ -112,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -121,10 +122,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+
+    # Packages
+    'south',
+
+    # Custom applications
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
