07ba56b47ac46bbe20c9b1096bfbeb71adb1bfa6
Novacha Man <novachalon@gmail.com>
2013-10-27 12:29:29 +1100

    Added basic url patterns to envoy_api application

---
+++ src/server/envoy_api/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'server.views.home', name='home'),
+    # url(r'^server/', include('server.foo.urls')),
+)
